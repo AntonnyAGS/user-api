6a0c0fd273d384c559789354dd1422dc63571a13
VitoriaPaula <48971522+VitoriaPaula@users.noreply.github.com>
2020-11-04 22:47:21 -0300

    Alterações by Vih

---
+++ src/main/java/com/projeto/faculdade/ColetaProjetos/services/impl/ClienteServiceImpl.java
@@ -0,0 +1,21 @@
+package com.projeto.faculdade.ColetaProjetos.services.impl;
+
+import com.projeto.faculdade.ColetaProjetos.model.ClienteModel;
+import com.projeto.faculdade.ColetaProjetos.services.IClienteService;
+import javax.inject.Named;
+
+@Named
+public class ClienteServiceImpl implements IClienteService {
+
+	/*	Podemos criar a interface Repository(que conversa com as classes @Entity)
+	*	aqui e dar Inject
+	*	@Inject
+	*	IClienteRepository clienteRepo;
+	*/
+	
+    @Override
+    public ClienteModel cadastrarCliente(ClienteModel cadastroClienteDomainModelRequest) {
+    	//DO SOME MAGIC!
+    	return null;
+    }
+}
